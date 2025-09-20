04e66c1d71c2eb357ff21d9cb58d9387456adde2
Nishant Jhade <168710192+nishant-x@users.noreply.github.com>
2025-09-20 14:03:06 +0530

    Update User.js

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -21,15 +21,18 @@ const userSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ['admin', 'agent'],
-    default: 'agent'
+    enum: ["admin", "agent"],
+    default: "agent"
   }
 });
 
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 12);
   next();
 });
 
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
