7093cfe5751b1200fbebd8f4e57c3fbb29a3319f
Nishant Jhade <168710192+nishant-x@users.noreply.github.com>
2025-09-20 13:38:39 +0530

    Update authRoutes.js

--- Backend/routers/authRoutes.js
+++ Backend/routers/authRoutes.js
@@ -6,4 +6,4 @@ const authController = require('../controllers/authController');
 router.post('/login', authController.login);
 router.post('/signup', authController.signup);
 
-module.exports = router;
+export default router;
